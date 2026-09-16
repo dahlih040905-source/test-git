bb2a8714eea67b5c8803252133d6e0d20deb45fb
dai040905 <dai040905@gmail.com>
2026-09-16 15:36:59 +0800

    feat: initial commit with app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+// 簡易計算機與字串處理小工具
+function add(a, b) {
+    return a + b;
+}
+
+function greet(name) {
+    return `Hello, ${name}! Welcome to Git demo.`;
+}
+
+console.log(greet("Developer"));
+console.log(`3 + 5 = ${add(3, 5)}`);
